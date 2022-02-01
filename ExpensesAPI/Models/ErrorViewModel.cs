e9070148ae16b6cc6c7b3e2d73549b26195e53f3
Noah Day <sheltielover432@gmail.com>
2022-01-31 21:16:56 -0500

    Create New Project

---
+++ ExpensesAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExpensesAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
